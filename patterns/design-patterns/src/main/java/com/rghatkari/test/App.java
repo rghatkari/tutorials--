db7cff94fe1c6b982f8eabecf84763d1c5df704a
Rajesh Ghatkari <rajesh.ghatkari@nwea.org>
2019-02-14 18:41:32 +0530

    Adding design patterns

---
+++ patterns/design-patterns/src/main/java/com/rghatkari/test/App.java
@@ -0,0 +1,13 @@
+package com.rghatkari.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
