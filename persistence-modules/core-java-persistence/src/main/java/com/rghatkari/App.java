a35c0e74764805a93d3f569439ba4b1ed5d4f99f
Rajesh Ghatkari <rajesh.ghatkari@nwea.org>
2019-02-19 14:07:31 +0530

    Adding persistence-modules

---
+++ persistence-modules/core-java-persistence/src/main/java/com/rghatkari/App.java
@@ -0,0 +1,13 @@
+package com.rghatkari;
+
+/**
+ * Persistence-modules!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        System.out.println( "Persistence modules!" );
+    }
+}
